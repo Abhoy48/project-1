c7cb5cd7324488275bc0710dc24671379e4a0b7b
Abhoy48 <abhoydas650@gmail.com>
2024-09-28 00:08:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+gsap.from("#img1", {
+    Delay:0.7,
+    opacity:0,
+    duration:1.8,
+    y:60
+})
+
+
+gsap.from("#img2", {
+    Delay:0.2,
+    opacity:0,
+    duration:2.1,
+    x:30
+})
+
+
+gsap.from("#img3", {
+    Delay:0.3,
+    opacity:0,
+    duration:1,
+    x:-60
+})
+
+
+gsap.from("#main h1", {
+    Delay:0.3,
+    opacity:0,
+    duration:1,
+    z:60
+})
+
+gsap.from("#nav h1", {
+    Delay:0.3,
+    duration:0.7,
+  
+})
+
+
+// gsap.from("#main>h1::after ", {
+//     Delay:0.3,
+//     opacity:0,
+//     duration:0.7,
+//     y:30
+// })
